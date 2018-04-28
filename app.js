fe0f61a476d40cf7b411128d9af42728114b2c2f
Anthony Duque <duquea95@gmail.com>
2018-04-28 13:54:19 -0400

    merging app.js

--- app.js
+++ app.js
@@ -2,7 +2,12 @@ var express = require('express');
 var app = express();
 var mustacheExpress = require('mustache-express');
 var bodyParser = require ('body-parser');
-
+var fs = require('fs');
+var Mta = require('mta-gtfs');
+var mta = new Mta({
+  key: '91a371c98bd07ad884c3e400c9e23a68', // only needed for mta.schedule() method
+  feed_id: 1                  // optional, default = 1
+});
 var port = 3000;
 
 app.engine('html', mustacheExpress());
@@ -12,10 +17,30 @@ app.use("/", express.static(__dirname + '/public'));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-// app.get(function(req,res)){
-//     res.render
-// }
+mta.status('subway').then(function (result) {
+	// result = JSON.stringify(result);
+	result = JSON.stringify(result).replace(/\\n|\\r/g, '');
 
-app.listen(port,function() {
-    console.log('We live!');
+
+	var regex = /(<([^>]+)>)/ig; //Regex for removing all html tags
+	var reg = new RegExp("[ ]+","g"); // Resgex for removing whitespaces
+    result = result.replace(reg," ");
+    //
+	result = result.replace(regex, ""); //Calling 1st regex
+    //
+    console.log(result);
+    //
+     // fs.writeFile("data.json", result, function(err) {
+     //     if (err) {
+     //         console.log(err);
+     //     }
+     // })
+ }).catch(function (err) {
+     console.log(err);
+ });
+
+
+app.listen(port,function(){
+
+    console.log('hello');
 })
--- node/app.js
+++ node/app.js
@@ -1,29 +1,28 @@
-var fs = require('fs');
-var Mta = require('mta-gtfs');
-var mta = new Mta({
-  key: '91a371c98bd07ad884c3e400c9e23a68', // only needed for mta.schedule() method
-  feed_id: 1                  // optional, default = 1
-});
-
-mta.status('subway').then(function (result) {
-	// result = JSON.stringify(result);
-	result = JSON.stringify(result).replace(/\\n|\\r/g, '');
-
-
-
-	var regex = /(<([^>]+)>)/ig; //Regex for removing all html tags
-	var reg = new RegExp("[ ]+","g"); // Resgex for removing whitespaces
-        result = result.replace(reg," ");
-	
-	 result = result.replace(regex, ""); //Calling 1st regex
-	 
-
-  fs.writeFile("data.json", result, function(err) {
-    if (err) {
-        console.log(err);
-    }
-});;
-}).catch(function (err) {
-  console.log(err);
-});
-
+// var fs = require('fs');
+// var Mta = require('mta-gtfs');
+// var mta = new Mta({
+//   key: '91a371c98bd07ad884c3e400c9e23a68', // only needed for mta.schedule() method
+//   feed_id: 1                  // optional, default = 1
+// });
+//
+// mta.status('subway').then(function (result) {
+// 	// result = JSON.stringify(result);
+// 	result = JSON.stringify(result).replace(/\\n|\\r/g, '');
+//
+//
+//
+// 	var regex = /(<([^>]+)>)/ig; //Regex for removing all html tags
+// 	var reg = new RegExp("[ ]+","g"); // Resgex for removing whitespaces
+//         result = result.replace(reg," ");
+//
+// 	 result = result.replace(regex, ""); //Calling 1st regex
+//
+//
+//   fs.writeFile("data.json", result, function(err) {
+//     if (err) {
+//         console.log(err);
+//     }
+// });;
+// }).catch(function (err) {
+//   console.log(err);
+// });
