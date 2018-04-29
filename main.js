d66a6b907fafe883ecd7be6bea980bc6fbfe3d4b
Anthony Duque <duquea95@gmail.com>
2018-04-29 10:51:00 -0400

    add api text

--- main.js
+++ main.js
@@ -1,13 +1,20 @@
 $(function(){
-    $('#test').click(function() {
-        var trainLine =
+    var line;
+    $('img').click(function() {
+        if(line == null){
+            console.log('hey')
+        };
+        line = $(this).attr("class"); //get train class
+        console.log(line);
         event.preventDefault();
         $.ajax({
             type: "GET",
             url: "http://localhost:3000/getStatus",
-            data: {line: '2'},
-            success: function() {
-                console.log($("#test"));
+            data: {line: line},
+            success: function(data) {
+                console.log(data);
+                console.log(data["text"]);
+                $(".train-status").append(data["text"]);
             },
             error: console.error
         });
