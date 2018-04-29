a488dfedc0b42e48c888f557aca76337fe483498
Anthony Duque <duquea95@gmail.com>
2018-04-29 11:28:13 -0400

    js state changes

--- main.js
+++ main.js
@@ -3,20 +3,27 @@ $(function(){
     $('img').click(function() {
         if(line == null){
             console.log('hey')
-        };
-        line = $(this).attr("class"); //get train class
-        console.log(line);
-        event.preventDefault();
-        $.ajax({
-            type: "GET",
-            url: "http://localhost:3000/getStatus",
-            data: {line: line},
-            success: function(data) {
-                console.log(data);
-                console.log(data["text"]);
-                $(".train-status").append(data["text"]);
-            },
-            error: console.error
-        });
+            line = $(this).attr("alt"); //get train class
+            $(".train-status-inner").empty();
+        }else{
+            console.log('set')
+            line = null;
+            line = $(this).attr("class"); //get train class
+            $(".train-status-inner").empty();
+        }
+            console.log(line);
+            event.preventDefault();
+            $.ajax({
+                type: "GET",
+                url: "http://localhost:3000/getStatus",
+                data: {line: line},
+                success: function(data) {
+                    console.log(data);
+                    console.log(line);
+                    $(".train-name").text(data["name"]);
+                    $(".train-status-inner").append(data["text"]);
+                },
+                error: console.error
+            });
     });
 });
