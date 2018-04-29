6ffa3999914829f122fe80f9ef6e64f9b6bce23c
Anthony Duque <duquea95@gmail.com>
2018-04-29 11:50:12 -0400

    edit

--- main.js
+++ main.js
@@ -11,15 +11,14 @@ $(function(){
             line = $(this).attr("class"); //get train class
             $(".train-status-inner").empty();
         }
-            console.log(line);
             event.preventDefault();
             $.ajax({
                 type: "GET",
                 url: "http://localhost:3000/getStatus",
                 data: {line: line},
                 success: function(data) {
-                    console.log(data);
-                    console.log(line);
+                    // console.log(data);
+                    // console.log(line);
                     $(".train-name").text(data["name"]);
                     $(".train-status-inner").append(data["text"]);
                 },
