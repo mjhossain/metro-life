7fc9ebba078cddf6459fd6d69146b5165526c6e2
zakir <mjhossainnyc@gmail.com>
2018-04-30 15:51:37 -0400

    Merged Anthony's Version with Master and added more css

--- main.js
+++ main.js
@@ -1,5 +1,22 @@
-// require(["app"], function () {
-//     $(function() {
-//         console.log("hello world");
-//     });
-// });
+$(function(){
+    var line;
+    $('img').click(function() {
+            line = null;
+            console.log('hey')
+            line = $(this).attr("alt"); //get train class
+            $(".train-status-inner").empty();
+            event.preventDefault();
+            $.ajax({
+                type: "GET",
+                url: "http://localhost:3000/getStatus",
+                data: {line: line},
+                success: function(data) {
+                    // console.log(data);
+                    // console.log(line);
+                    $(".train-name").text(data["name"]);
+                    $(".train-status-inner").append(data["text"]);
+                },
+                error: console.error
+            });
+    });
+});
